3106fd71deb64fb85df3332670b71cdf7570a637
Deepikakolli4 <deepikakolli2004@gmail.com>
2024-06-21 13:38:06 +0530

    node practise
    Signed-off-by: Deepikakolli4 <deepikakolli2004@gmail.com>

---
+++ npm.js
@@ -0,0 +1,7 @@
+//  var generateName = require('sillyname');
+// var sillyName = generateName();
+//  console.log(`my name is ${sillyName}`); 
+import generateName from "sillyname";
+
+var sillyName = generateName();
+console.log(`My name is ${sillyName}`);
