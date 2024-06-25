dae568c72327c5b9fcafb9ea574c39977b823a88
Deepikakolli4 <deepikakolli2004@gmail.com>
2024-06-25 14:16:52 +0530

    middleware
    Signed-off-by: Deepikakolli4 <deepikakolli2004@gmail.com>

--- expressprac/index.js
+++ expressprac/index.js
@@ -2,10 +2,24 @@ import express from "express";
 const app=express();
 const port=3000;
 app.get("/",(req,res)=>{
+    //get the resource;
    res.send("<h1>HELLO PAGE</h1>");
 })
-app.get("/about",(req,res)=>{
-    res.send("<h1>ABOUT PAGE</h1>");
+app.post("/register",(req,res)=>{
+    //sending resource
+    res.send(201);
+ })
+ app.put("/user/tom",(req,res)=>{
+    //replacing the complete resource with new resource
+    res.sendStatus(200);
+ })
+ app.patch("/user/tom",(req,res)=>{
+    //replacing only the damaged or useless resource with new resource
+    res.send(200);
+ })
+ app.delete("/user/tom",(req,res)=>{
+    //deleting the entire resource
+    res.send(200);
  })
 app.listen(port,()=>{
     console.log(`your server is running at ${port}`);
